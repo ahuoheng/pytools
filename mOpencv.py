83c1dde3b6080de954a632280355fe2d75ef32e0
chenwh <a_huo@qq.com>
2020-02-26 13:10:40 +0800

    cv

--- mOpencv.py
+++ mOpencv.py
@@ -4,3 +4,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 
+def imread(imgfile):
+    cv2.imread(imgfile)
+
+def imshow(wname,img):
+    cv2.imshow(wname, img)
+    cv2.waitKey(1)
+
+img=imread('testdata/face.jpg')
+imshow("w",img)
+cv2.waitKey()
